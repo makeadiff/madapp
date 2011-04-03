665e22d09bcc7f468f8732ab7ed56c335f90e7f4
binnyva <binnyva@gmail.com>
2011-04-03 06:36:14 +0000

    Pluralized all words
    git-svn-id: http://svn.binnyva.com/madapp/trunk@92 1b907b55-5aaf-4fb5-97f6-095844034206

--- system/application/views/dashboard/dashboard.php
+++ system/application/views/dashboard/dashboard.php
@@ -9,7 +9,7 @@
 <div id="quick" class="clear" style="margin-top:-15px;">
 	<?php if($this->user_auth->get_permission('city_index')) { ?>
 	<div class="quickLink"> <a href="<?= site_url('city/') ?>">
-	<img src="<?php echo base_url(); ?>images/ico/city.jpeg" alt="" /> <span>City</span></a></div>
+	<img src="<?php echo base_url(); ?>images/ico/city.jpeg" alt="" /> <span>Cities</span></a></div>
 	<?php } ?>
 	
 	<?php if($this->user_auth->get_permission('project_index')) { ?>
@@ -31,12 +31,12 @@
 	
 	<?php if($this->user_auth->get_permission('report_index')) { ?>
 	<div class="quickLink"> <a href="<?= site_url('report/') ?>">
-	<img src="<?php echo base_url(); ?>images/ico/reports.png" alt="" /> <span>Report</span></a></div>
+	<img src="<?php echo base_url(); ?>images/ico/reports.png" alt="" /> <span>Reports</span></a></div>
 	<?php } ?>
 	
 	<?php if($this->user_auth->get_permission('classes_madsheet')) { ?>
 	<div class="quickLink"> <a href="<?= site_url('classes/madsheet') ?>">
-	<img src="<?php echo base_url(); ?>images/ico/stock_new-spreadsheet.png" alt="" /> <span>MADSheet</span></a></div>
+	<img src="<?php echo base_url(); ?>images/ico/stock_new-spreadsheet.png" alt="" /> <span>MAD Sheet</span></a></div>
 	<?php } ?>
 	
 	<br />
@@ -49,12 +49,12 @@
 	
 	<?php if($this->user_auth->get_permission('exam_index')) { ?>
 	<div class="quickLink"> <a href="<?= site_url('exam/manage_exam') ?>">
-	<img src="<?php echo base_url(); ?>images/ico/exam_icon.jpg" alt="" /> <span>Exams </span></a></div>
+	<img src="<?php echo base_url(); ?>images/ico/exam_icon.jpg" alt="" /> <span>Exams</span></a></div>
 	<?php } ?>
 	
 	<?php if($this->user_auth->get_permission('exam_marks_index')) { ?>
 	<div class="quickLink"> <a href="<?= site_url('exam/exam_score') ?>">
-	<img src="<?php echo base_url(); ?>images/ico/mark.png" alt="" /> <span>Exam Mark</span></a></div>
+	<img src="<?php echo base_url(); ?>images/ico/mark.png" alt="" /> <span>Exam Marks</span></a></div>
 	<?php } ?>
 		
 	<br />

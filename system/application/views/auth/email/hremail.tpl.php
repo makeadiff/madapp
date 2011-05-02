a365b3a5ab809ea938f963f02f5c7ce6b51f9732
rabeesh <rabeesh@orysis.in>
2011-05-02 13:19:53 +0000

    git-svn-id: http://svn.binnyva.com/madapp/trunk@125 1b907b55-5aaf-4fb5-97f6-095844034206

---
+++ system/application/views/auth/email/hremail.tpl.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+	<h1><?php echo $new_registration_notification;?></h1>
+	<p>Student Name:<?php $name;?> </p>
+    <p>Student Email:<?php echo $email; ?></p>
+    <p>Student PhoneNumber: <?php echo $phone; ?></p>
+</body>
+</html>

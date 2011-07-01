037a28c6d8dd022f39ef16f240f147e53950ab09
binnyva <binnyva@gmail.com>
2011-07-01 04:33:47 +0000

    Made the Login form more... madapp like
    git-svn-id: http://svn.binnyva.com/madapp/trunk@171 1b907b55-5aaf-4fb5-97f6-095844034206

--- system/application/views/auth/login.php
+++ system/application/views/auth/login.php
@@ -2,63 +2,26 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
 <head>
 <title>MADApp Login</title>
-<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/g.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/l.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/bk.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/r.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/custom.css" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/validation.css" />
-<style type="text/css">
-body {
-	text-align:center;
-	font-size:1.5em;
-}
-form {
-	background-color:#eee;
-	width:400px;
-	padding:10px;
-	margin:100px auto;
-	text-align:left;
-}
-input {
-	font-size:1em;
-}
-.small {
-	font-size:16px;
-}
-</style>
-
+<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/camp/master.css" />
 </head>
-<body id="pageLogin">
+<body id="pg-login">
+
+<div class="login-box">
+<h1>MADApp Login</h1>
 <div class="field" style="color:#CC0000; text-align:center; margin-top:50px;"><?php echo $message;?></div>
-    
+
 <?php echo form_open("auth/login");?>
-<fieldset>
-<legend>MADApp Login</legend>
 
-<div class="field"> 
-<label for="email">Email</label><br />
-<?php echo form_input($email); ?>
-</div><br /><br />
+<ul class="form login-form">
+<li><label for="email">Email</label><?php echo form_input($email); ?></li>
+<li><label for="password">Password</label><?php echo form_input($password); ?></li>
+<li><label for="remember" class="small">Remember Me</label><?php echo form_checkbox(array('name'=>'remember','id'=>'remember','value'=>'1', 'checked'=>true));?></li>
+<li><a class="small" href="<?php echo site_url('auth/forgotpassword') ?>">Forgot Password?</a><?php echo form_submit('submit', 'Login', 'class="button green"');?></li>
+<li></li>
+</ul>
 
-<div class="field">
-<label for="password">Password</label><br />
-<?php echo form_input($password); ?>
-</div><br /><br />
-
-<div class="field">
-<label for="remember" class="small">Remember Me</label>
-<?php echo form_checkbox(array('name'=>'remember','id'=>'remember','value'=>'1', 'checked'=>FALSE));?>
-</div><br />
-
-<div class="field">
-<?php echo form_submit('submit', 'Login');?><br />
-<a class="small" href="<?php echo site_url('auth/forgotpassword') ?>">Forgot Password?</a>
-
-</div>
-
-</fieldset>
 <?php echo form_close();?>
+</div>
 
 </body>
 

b267d0c0745b13b743ecb166ed8d12e6315db4ce
binnyva <binnyva@gmail.com>
2011-10-18 18:08:21 +0000

    git-svn-id: http://svn.binnyva.com/madapp/trunk@272 1b907b55-5aaf-4fb5-97f6-095844034206

---
+++ system/application/controllers/debug.php
@@ -0,0 +1,44 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 4.3.2 or newer
+ *
+ * @package         MadApp
+ * @author          Rabeesh
+ * @copyright       Copyright (c) 2008 - 2010, OrisysIndia, LLP.
+ * @link            http://orisysindia.com
+ * @since           Version 1.0
+ * @filesource
+ */
+class Debug extends Controller {
+   	function Debug() {
+        parent::Controller();
+		$this->load->library('session');
+		$this->load->library('navigation');
+        $this->load->library('user_auth');
+		$this->load->library('validation');
+		$this->load->helper('url');
+        $this->load->helper('form');
+		$this->load->model('center_model');
+		$this->load->model('project_model');
+		$this->load->model('users_model');
+		$this->load->model('city_model');
+	}
+
+	function delete_students_in_center() {
+		$centers = array(7,22,23,24,10,11,12,36);
+		
+		$student_ids = $this->users_model->db->query("SELECT id FROM `Student` WHERE center_id IN(".implode(',',$centers).")")->result();
+		print "Total Students: " . count($student_ids) . '<br />';
+		//return;
+		
+		foreach($student_ids as $row) {
+			$id = $row->id;
+			print "$id,";
+			$this->db->query("DELETE FROM Student WHERE id=$id");
+			$this->db->query("DELETE FROM StudentLevel WHERE student_id=$id");
+			$this->db->query("DELETE FROM StudentClass WHERE student_id=$id");
+		}
+	}
+}

a357a662c33505f026c6ee4e7e513a4f8e064dec
binnyva <binnyva@gmail.com>
2012-05-08 06:15:47 +0000

    Updating the error pages.
    git-svn-id: http://svn.binnyva.com/madapp/trunk@334 1b907b55-5aaf-4fb5-97f6-095844034206

--- system/application/errors/error_404.php
+++ system/application/errors/error_404.php
@@ -1,34 +1,11 @@
-<html>
-<head>
-<title>404 Page Not Found</title>
-<style type="text/css">
+<?php
+$ci = &get_instance();
+$ci->load->view('layout/header', array('title'=>'MADApp :: 404 - Page Not found')); ?>
 
-body {
-background-color:	#fff;
-margin:				40px;
-font-family:		Lucida Grande, Verdana, Sans-serif;
-font-size:			12px;
-color:				#000;
-}
+<h1><?php echo $heading; ?></h1><br /><br />
 
-#content  {
-border:				#999 1px solid;
-background-color:	#fff;
-padding:			20px 20px 12px 20px;
-}
+<p>Please check the URL you used.</p>
 
-h1 {
-font-weight:		normal;
-font-size:			14px;
-color:				#990000;
-margin: 			0 0 4px 0;
-}
-</style>
-</head>
-<body>
-	<div id="content">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
-</html>
+<?php echo $message; ?>
+
+<?php $ci->load->view('layout/footer');
--- system/application/errors/error_db.php
+++ system/application/errors/error_db.php
@@ -1,34 +1,16 @@
-<html>
-<head>
-<title>Database Error</title>
-<style type="text/css">
+<?php 
+$ci = &get_instance();
+$user_id = $ci->session->userdata('id');
+if($user_id) $ci->load->view('layout/header', array('title'=>'MADApp :: Database Error'));
+else $this->load->view('layout/thickbox_header');
+?>
 
-body {
-background-color:	#fff;
-margin:				40px;
-font-family:		Lucida Grande, Verdana, Sans-serif;
-font-size:			12px;
-color:				#000;
-}
+<h1><?php echo $heading; ?></h1>
 
-#content  {
-border:				#999 1px solid;
-background-color:	#fff;
-padding:			20px 20px 12px 20px;
-}
+<p>There is an error in the application. Our people are looking into it.</p>
 
-h1 {
-font-weight:		normal;
-font-size:			14px;
-color:				#990000;
-margin: 			0 0 4px 0;
-}
-</style>
-</head>
-<body>
-	<div id="content">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
-</body>
-</html>
+<?php echo $message; ?>
+
+<?php
+if($user_id) $this->load->view('layout/footer');
+else $this->load->view('layout/thickbox_footer');

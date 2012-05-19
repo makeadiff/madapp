1297ebdd2e9719af2a5b24253489f32bad987e79
binnyva <binnyva@gmail.com>
2012-05-19 02:13:47 +0000

    Better error pages.
    git-svn-id: http://svn.binnyva.com/madapp/trunk@341 1b907b55-5aaf-4fb5-97f6-095844034206

---
+++ system/application/errors/include/footer.php
@@ -0,0 +1,34 @@
+ 
+      </div>
+<div id="push">&nbsp;</div>
+<div class="clear"></div>
+<div style="float:right;" class="sidebar" id="sidebar" >
+<!-- MODULE ENDS -->
+</div>
+     </div>
+    </div>
+
+    <!-- BODY ENDS -->
+    <div class="footer">
+     <div class="line"></div>
+    </div>
+</div>
+
+<script type="text/javascript" charset="utf-8">
+  var is_ssl = ("https:" == document.location.protocol);
+  var asset_host = is_ssl ? "https://s3.amazonaws.com/getsatisfaction.com/" : "http://s3.amazonaws.com/getsatisfaction.com/";
+  document.write(unescape("%3Cscript src='" + asset_host + "javascripts/feedback-v2.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
+<script type="text/javascript" charset="utf-8">
+  var feedback_widget_options = {};
+  feedback_widget_options.display = "overlay";  
+  feedback_widget_options.company = "mad";
+  feedback_widget_options.placement = "bottom";
+  feedback_widget_options.color = "#222";
+  feedback_widget_options.style = "problem";
+  var feedback_widget = new GSFN.feedback_widget(feedback_widget_options);
+</script>
+</body>
+</html>
+
---
+++ system/application/errors/include/header.php
@@ -0,0 +1,47 @@
+<?php
+$base_url = 'http://localhost/Projects/Madapp/CI/trunk';
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>MADApp :: <?php echo $title ?></title>
+<link rel="stylesheet" type="text/css" href="<?php echo $base_url ?>/css/camp/master.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $base_url ?>/css/validation.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $base_url ?>/css/style.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo $base_url ?>/images/silk_theme.css" />
+<script type="text/javascript" src="<?php echo $base_url ?>/js/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo $base_url ?>/js/application.js"></script>
+<script type="text/javascript" src="<?php echo $base_url ?>/js/jquery.tablesorter.min.js"></script>
+</head>
+
+<body id="pg-dashboard-dashboard_viewx">
+<div class="wrap">
+    <!-- HEAD BEGINS -->
+    <div class="head">
+     <div class="line">	
+       <div class="logo left"><a href="<?php echo $base_url ?>/index.php/dashboard/dashboard_view">MADAp&#946;</a></div>
+       <div class="welcome right">
+             
+       <a href="<?php echo $base_url ?>/index.php/auth/logout" class="tool" style="margin-left: 10px; float:right;">Logout</a>
+		</div>
+     </div>
+     
+     <div class="line">
+       <h1>MADApp :: 404 - Page Not found</h1>
+
+              <div class="tools right">
+       	 <a href="<?php echo $base_url ?>/index.php/dashboard/dashboard_view" class="dash active" title="Dashboard">Dashboard</a>
+         <a href="<?php echo $base_url ?>/index.php/classes/madsheet#" class="sheet  " title="MAD Sheet">MAD Sheet</a>
+         <a href="<?php echo $base_url ?>/index.php/classes#" class="class " title="Classes">Classes</a>
+         <a href="<?php echo $base_url ?>/index.php/user/edit_profile" class="profile" title="Profile">Profile</a>
+         <!-- <a href="#" class="setting" title="Settings">Settings</a> -->
+       </div>
+
+     </div>
+     
+    </div>
+    <!-- HEAD ENDS -->
+    <!-- BODY BEGINS -->
+    <div class="line">
+     <div class="main">
+

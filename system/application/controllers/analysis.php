157177d538f67b679ad788313d1e527ca8d93404
binnyva <binnyva@gmail.com>
2012-05-27 11:40:18 +0000

    Monthly review uses old data.
    git-svn-id: http://svn.binnyva.com/madapp/trunk@347 1b907b55-5aaf-4fb5-97f6-095844034206

--- system/application/controllers/analysis.php
+++ system/application/controllers/analysis.php
@@ -186,7 +186,7 @@ class Analysis extends Controller {
 		$data['student_count']= count($this->kids_model->getkids_details()->result());
 		$data['teacher_count']= count($this->user_model->search_users(array('user_group'=>9))); // 9 = Teacher
 		
-		$data['months'] = get_month_list();
+		$data['months'] = array('2011-04', '2011-05', '2011-06', '2011-07', '2011-08', '2011-09', '2011-10', '2011-11', '2011-12', '2012-01', '2012-02', '2012-03', ); get_month_list();
 		foreach($data['months'] as $year_month) {
 			$data['review'][$year_month] = idNameFormat($this->review_model->get_monthly_review($year_month, $this->session->userdata('city_id')), array('name'));
 		}

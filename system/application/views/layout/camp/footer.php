512e727ed1708f2a6afca3a268b2cae5a6f14fd3
binnyva <binnyva@gmail.com>
2012-11-25 15:09:04 +0000

    Added tracking code
    git-svn-id: http://svn.binnyva.com/madapp/trunk@434 1b907b55-5aaf-4fb5-97f6-095844034206

--- system/application/views/layout/camp/footer.php
+++ system/application/views/layout/camp/footer.php
@@ -14,22 +14,18 @@
 </div>
 <a id="fdbk_tab" class="fdbk_tab_bottom" style="background-color:#222" href="https://getsatisfaction.com/mad/topics/new">FEEDBACK</a>
 
-<!--
-<script type="text/javascript" charset="utf-8">
-  var is_ssl = ("https:" == document.location.protocol);
-  var asset_host = is_ssl ? "https://s3.amazonaws.com/getsatisfaction.com/" : "http://s3.amazonaws.com/getsatisfaction.com/";
-  document.write(unescape("%3Cscript src='" + asset_host + "javascripts/feedback-v2.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
+<script type="text/javascript">
 
-<script type="text/javascript" charset="utf-8">
-  var feedback_widget_options = {};
-  feedback_widget_options.display = "overlay";  
-  feedback_widget_options.company = "mad";
-  feedback_widget_options.placement = "bottom";
-  feedback_widget_options.color = "#222";
-  feedback_widget_options.style = "problem";
-  var feedback_widget = new GSFN.feedback_widget(feedback_widget_options);
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5816278-6']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 </script>
--->
 </body>
 </html>

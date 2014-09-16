c52f230bee17f26cff42f6d7d10ed8c931942231
Binny V A <binnyva@gmail.com>
2014-09-16 12:51:18 +0530

    Making 360s viewable

--- system/application/views/review/select_people.php
+++ system/application/views/review/select_people.php
@@ -14,7 +14,7 @@ $this->load->view('layout/header', array('title' => "Review Sheet: Select Person
 <tr><th>Name</th><th>City</th><th>Region</th><th>Vertical</th><th>Group</th></tr>
 <?php foreach ($fellows as $person) { ?>
 <tr>
-<td><a href="<?php echo site_url('review/review_fellow/'.base64_encode($person->id).'/1/no360'); ?>"><?php echo $person->name ?></a></td>
+<td><a href="<?php echo site_url('review/review_fellow/'.base64_encode($person->id)); ?>"><?php echo $person->name ?></a></td>
 <td><?php echo $person->city_name ?></td>
 <td><?php echo $all_regions[$person->region_id] ?></td>
 <td><?php echo $all_verticals[$person->vertical_id] ?></td>

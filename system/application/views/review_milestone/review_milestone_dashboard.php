8e96ec86a2cacc8e711e72a932305b3d5022744b
Sanjay Thomas Alanoly <me@sanjayalanoly.com>
2014-10-24 12:56:14 +0530

    Added 360 and my milestones new icon

--- system/application/views/review_milestone/review_milestone_dashboard.php
+++ system/application/views/review_milestone/review_milestone_dashboard.php
@@ -7,7 +7,12 @@
 
             <?php if($this->user_auth->get_permission('milestone_my')) { ?>
                 <div class="col-md-4 col-sm-6 text-center"> <a  class='btn btn-primary btn-dash' href="<?php echo site_url('review/my_milestones') ?>">
-                        <img src="<?php echo base_url(); ?>images/flat_ui/my_milestones.png" alt="" /> <br>My<br>Milestones</a></div>
+                        <img class="dash" src="<?php echo base_url(); ?>images/flat_ui/my_milestones_new.png" alt="" /> <br>My<br>Milestones</a></div>
+            <?php } ?>
+
+            <?php if($this->user_auth->get_permission('milestone_list')) { ?>
+                <div class="col-md-4 col-sm-6 text-center"> <a  class='btn btn-primary btn-dash' href="<?php echo site_url('review/milestone_select_people/') ?>">
+                        <img src="<?php echo base_url(); ?>images/flat_ui/assign_milestones.png" alt="" /> <br>Assign<br>Milestones</a></div>
             <?php } ?>
 
             <?php if($this->user_auth->get_permission('review_data_my')) { ?>
@@ -15,9 +20,19 @@
                         <img src="<?php echo base_url()?>/images/flat_ui/my_review.png" alt="" /><br>My Review<br>Sheet</a></div>
             <?php } ?>
 
+            <?php if($this->user_auth->get_permission('review_select_person')) { ?>
+                <div class="col-md-4 col-sm-6 text-center"> <a  class='btn btn-primary btn-dash' href="<?php echo site_url('review/select_people/') ?>">
+                        <img src="<?php echo base_url(); ?>images/flat_ui/view_review_sheets.png" alt="" /> <br>View Reivew<br>Sheets</a></div>
+            <?php } ?>
+
             <?php if($this->user_auth->get_permission('okr_my')) { ?>
                 <div class="col-md-4 col-sm-6 text-center"> <a class='btn btn-primary btn-dash' href="http://makeadiff.in/apps/okr/">
                         <img src="<?php echo base_url()?>/images/flat_ui/okr.png" alt="" /><br>OKR</a></div>
+            <?php } ?>
+
+            <?php if($this->user_auth->get_permission('reimbursement')) { ?>
+                <div class="col-md-4 col-sm-6 text-center"> <a class='btn btn-primary btn-dash' href="http://makeadiff.in/apps/prism/public/">
+                        <img class="dash" src="<?php echo base_url()?>/images/flat_ui/360.png" alt="" /><br>MAD 360</a></div>
             <?php } ?>
 
             <?php /*if($this->user_auth->get_permission('happiness_index')) { */?>
@@ -25,15 +40,9 @@
                         <img src="<?php echo base_url(); ?>images/flat_ui/happiness_index.png" alt="" /> <br>Happiness<br>Index Status</a></div>
             <?php /*} */?>
 
-            <?php if($this->user_auth->get_permission('milestone_list')) { ?>
-                <div class="col-md-4 col-sm-6 text-center"> <a  class='btn btn-primary btn-dash' href="<?php echo site_url('review/milestone_select_people/') ?>">
-                        <img src="<?php echo base_url(); ?>images/flat_ui/assign_milestones.png" alt="" /> <br>Assign<br>Milestones</a></div>
-            <?php } ?>
 
-            <?php if($this->user_auth->get_permission('review_select_person')) { ?>
-                <div class="col-md-4 col-sm-6 text-center"> <a  class='btn btn-primary btn-dash' href="<?php echo site_url('review/select_people/') ?>">
-                        <img src="<?php echo base_url(); ?>images/flat_ui/view_review_sheets.png" alt="" /> <br>View Reivew<br>Sheets</a></div>
-            <?php } ?>
+
+
 
 
         </div>

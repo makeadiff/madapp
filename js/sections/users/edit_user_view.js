4b28d7c4c7f2525cf7ef51d4af510fbdea87cca7
Binny V A <binnyva@gmail.com>
2015-11-06 19:12:56 +0530

    Small bug fix for uninducted attrition

--- js/sections/users/edit_user_view.js
+++ js/sections/users/edit_user_view.js
@@ -3,17 +3,10 @@ function init() {
 		var today = new Date();
 		// var day_yyyy_mm_dd = (today.getYear() + 1900) + "-" + String("00" + today.getMonth()).slice(-2) + "-" + String("00" + today.getDay()).slice(-2);
 		var day_yyyy_mm_dd = today.toISOString().slice(0,10);
-		if(this.value == "let_go") {
+
+
+		if(this.value == "let_go" || this.value == "alumni" || this.value == "left_before_induction") {
 			$("#exit-interview-feedback").show();
-			if($("#left_on").val() == "0000-00-00" || $("#left_on").val() == "") 
-				$("#left_on").val(day_yyyy_mm_dd);
-			else {
-				if(confirm("Update left on date to today?")) {
-					$("#left_on").val(day_yyyy_mm_dd);	
-				}
-			}
-		
-		} else if(this.value == "alumni") {
 			if($("#left_on").val() == "0000-00-00" || $("#left_on").val() == "") 
 				$("#left_on").val(day_yyyy_mm_dd);
 			else {

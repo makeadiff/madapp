b51c1b71e3666308413550fc1e1ca90fca84c917
Binny V A <binnyva@gmail.com>
2016-07-12 12:54:21 +0530

    Making reason for leaving mandatory

--- system/application/views/kids/popups/kids_delete_view.php
+++ system/application/views/kids/popups/kids_delete_view.php
@@ -1,4 +1,19 @@
 <?php $this->load->view('layout/thickbox_header'); ?>
+<script type="text/javascript">
+function init() {
+	$("#formEditor").submit(check);
+}
+
+function check(e) {
+	if(!($("#reason_for_leaving").val())) {
+		e.stopPropagation();
+		alert("Please make sure you have a reason for leaving.");
+		return false;
+	}
+	return true;
+}
+
+</script>
 <h2>Delete Student '<?php echo $name ?>'</h2>
 
 <form class="mainForm clear" id="formEditor" action="<?php echo site_url('kids/delete_student/' . $id)?>" method="post">
@@ -7,7 +22,7 @@
 <input type="hidden" value="<?php echo $id; ?>"  id="id" name="id" />
 <input id="btnSubmit" class="button green" type="submit" value="Delete Student" />
 
-<a href="<?=site_url('kids/manageaddkids')?>" class="sec-action">Cancel</a>
+<a href="<?php echo site_url('kids/manageaddkids'); ?>" class="sec-action">Cancel</a>
 </form>
 
 <?php $this->load->view('layout/thickbox_footer'); ?>

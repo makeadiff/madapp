b5a24e3f07ae3a33194c7d226339a3608e2ff554
Binny V A <binnyva@gmail.com>
2017-08-12 15:35:11 +0530

    Added Intercom to footer

--- system/application/views/auth/login.php
+++ system/application/views/auth/login.php
@@ -60,7 +60,15 @@ if(!empty($message['error'])) { ?>
 
 <script src="<?php echo base_url()?>js/jquery-1.9.0.js"></script>
 <script src="<?php echo base_url()?>js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url()?>js/uservoice.js"></script>
+<script>
+  window.intercomSettings = {
+    app_id: "xnngu157",
+    name: "", // Full name
+    email: "", // Email address
+    // created_at: 0 // Signup date as a Unix timestamp
+  };
+  </script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/xnngu157';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
 
 </body>
 

427f9c08b021229eda4e49e0b54cb484ffd45f08
Binny V A <binnyva@gmail.com>
2019-02-25 20:53:40 +0530

    Removed the captcha validate

--- system/application/controllers/common.php
+++ system/application/controllers/common.php
@@ -59,19 +59,19 @@ class Common extends Controller {
 
 			} else {
 
-				require('system/vendor/autoload.php');
-				$secret = '6Le_7hsTAAAAAG8-PiI757qs4LQEzQ28TZOgL-vJ';
-				$recaptcha = new \ReCaptcha\ReCaptcha($secret);
-				$gRecaptchaResponse = $_REQUEST['g-recaptcha-response'];
-				$remoteIp = $_SERVER['REMOTE_ADDR'];
+				// require('system/vendor/autoload.php');
+				// $secret = '6Le_7hsTAAAAAG8-PiI757qs4LQEzQ28TZOgL-vJ';
+				// $recaptcha = new \ReCaptcha\ReCaptcha($secret);
+				// $gRecaptchaResponse = $_REQUEST['g-recaptcha-response'];
+				// $remoteIp = $_SERVER['REMOTE_ADDR'];
 
-				$resp = $recaptcha->verify($gRecaptchaResponse, $remoteIp);
-				if ($resp->isSuccess()) {
-				    // verified!
-				} else {
-					print "Your application has triggered our spam detection algorithm. Click on the back button then make sure you have the \"I'm not a robot\" checkbox ticked.";
-					return false; // Spam.
-				}
+				// $resp = $recaptcha->verify($gRecaptchaResponse, $remoteIp);
+				// if ($resp->isSuccess()) {
+				//     // verified!
+				// } else {
+				// 	print "Your application has triggered our spam detection algorithm. Click on the back button then make sure you have the \"I'm not a robot\" checkbox ticked.";
+				// 	return false; // Spam.
+				// }
 
 				// Check for spam...
 				if((stripos($data['address'], '<a ') !== false) or (stripos($data['address'], 'http://') !== false) or (stripos($data['address'], 'https://') !== false)) {					// There is a link in the address field. Spam.

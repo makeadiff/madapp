48f7359b34f51ac551f6c03b8cfd72a770a46bf5
Mohit Gupta <mohitgupta1918@gmail.com>
2019-11-26 16:17:19 +0530

    removed options from attendance managementy

--- system/application/views/edsupport/attendance_management.php
+++ system/application/views/edsupport/attendance_management.php
@@ -11,16 +11,16 @@
                         <img src="<?php echo base_url(); ?>images/flat_ui/credit_history.png" alt="" /> <br>My Credit<br>History</a></div>
             <?php } ?>
 
-            <?php if($this->user_auth->get_permission('classes_batch_view')) { ?>
+            <!-- <?php if($this->user_auth->get_permission('classes_batch_view')) { ?>
                 <div class="col-md-3 col-sm-6 text-center"> <a  class='btn btn-primary btn-dash' href="<?php echo site_url('classes/batch_view') ?>">
                         <img src="<?php echo base_url(); ?>images/flat_ui/batch_view.png" alt="" /><br>Mark Attendance<br>(Mentor View)</a></div>
-            <?php } ?>
+            <?php } ?> -->
 
 
-            <?php if($this->user_auth->get_permission('classes_index')) { ?>
+            <!-- <?php if($this->user_auth->get_permission('classes_index')) { ?>
                 <div class="col-md-3 col-sm-6 text-center"> <a  class='btn btn-primary btn-dash' href="<?php echo site_url('classes/') ?>">
                         <img src="<?php echo base_url(); ?>images/flat_ui/classes.png" alt="" /> <br>Classes</a></div>
-            <?php } ?>
+            <?php } ?> -->
 
             <?php if($this->user_auth->get_permission('classes_madsheet')) { ?>
                 <div class="col-md-3 col-sm-6 text-center"> <a  class='btn btn-primary btn-dash' href="<?php echo site_url('classes/madsheet') ?>">
